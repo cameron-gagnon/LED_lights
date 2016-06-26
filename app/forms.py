e95f87618743b4ac7a552fff2f40bf10b5061b18
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-06-25 22:12:58 -0400

    Add bootstrap and remove login page until done

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,5 @@
-from wtforms import StringField, BooleanField, Form
-from wtforms.validators import DataRequired
+from wtforms import BooleanField, Form
 
 class LoginForm(Form):
 
-    username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from super_secret import USERNAME, PASSWORD
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,3 +7,11 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'asdf1234asdf1234'
+
+MAIL = {
+    'MAIL_SERVER':   'localhost',
+    'MAIL_PORT':     25,
+    'MAIL_USERNAME': USERNAME,
+    'MAIL_PASSWORD': PASSWORD,
+    'ADMINS': ['cameron.gagnon@gmail.com'],
+}
