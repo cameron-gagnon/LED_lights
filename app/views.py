da11f1304d0de5576c1c2eb989178220a9332704
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-18 10:00:47 -0400

    Almost added ability to POST a signal
    Need to make executable to send signal to arduino
    Need to fix POSTed values and how they're posted to the URL

--- app/views.py
+++ app/views.py
@@ -6,3 +6,12 @@ import time
 @app.route('/index')
 def index():
     return render_template('index.html', title='Pixelate')
+
+@app.route('/sendsignal/<signal>', methods=['POST'])
+def send_signal(signal):
+    print 'Sending signal: %d ' % (signal)
+
+    retCode = subprocess.call(['./pixelated', signal])
+
+    if not retCode:
+        print 'ERROR: exited with status: %d ' % retCode
