203d3172c1d194a891562b0be222f8e1572e0ae9
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-22 09:45:45 -0400

    accepts port number CLI argument

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 #! /usr/bin/env python2.7
 from app import app
-app.run(debug=True, host='0.0.0.0')
+import sys
+
+def getPort():
+    port = 80
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+    return int(port)
+
+
+app.run(debug=True, host='0.0.0.0', port = getPort())
