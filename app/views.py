4c5320ef6ba626e3448cb2129df46fe9e44c63b4
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-11-08 02:58:24 -0500

    update MVC workings, add multiprocessing for LED functions

--- app/views.py
+++ app/views.py
@@ -1,20 +1,30 @@
 from flask import render_template, flash, redirect, request, session, url_for
 from app import app
 import time
-import subprocess
+from multiprocessing import Process
+from LED_strip import strip, p, handler
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # Intialize the library (must be called once before other functions).
     return render_template('index.html', title='Pixelate')
 
 @app.route('/signal/<opcode>')
 def signal(opcode):
-    retCode = subprocess.call(['sudo', './pixelate_send', opcode])
+    global p
+    if (p.is_alive()):
+        print "Killing process"
+        p.terminate()
+
+    p = Process(target=send_signal, args=(opcode,))
+    p.start()
 
     # return 0 from main in pixelate_send.cpp so anything non-zero
     # prolly means we encountered errors
-    if retCode:
-        print 'ERROR: exited with status: %d ' % retCode
     return redirect( url_for('index') )
+
+
+def send_signal(opcode):
+    handler.send(strip, opcode)
