b27deac53e4f64e7f20e51c0dbcaf997c3ba643f
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-11-21 04:01:17 -0500

    Refactor handler to receive keywords instead of hex opcodes

--- app/LED_strip/handler.py
+++ app/LED_strip/handler.py
@@ -1,57 +1,54 @@
 class Handler(object):
 
-    KITCHEN_MASK = 0x000F
-    TV_WALL_MASK = 0x00F0
-    COUCH_WALL_MASK = 0x0F00
-
-    ON = 0x1
-    OFF = 0x0
-
-    THEATER_CHASE = 0x2
-    RAINBOW = 0x4
-    COLOR_WIPE = 0x8
-
     def __init__(self):
         pass
 
     def send(self, strip, opcode):
         print "Sending opcode: ", opcode
-        op = int(opcode, 16)
+        opcode = opcode.lower()
+        res = self.lookup.get(opcode, self.off)
+        res()
 
-        if (op & self.KITCHEN_MASK):
-            op &= self.KITCHEN_MASK
-            if (op == self.ON):
-                print "Sending kitchen on"
-                strip.on()
+    def theater_chase(self):
+        while (True):
+            strip.theaterChase(strip.BRIGHT_WHITE)
+            strip.theaterChase(strip.RED)
+            strip.theaterChase(strip.BLUE)
+            strip.theaterChase(strip.GREEN)
 
-            elif (op == self.OFF):
-                print "Sending kitchen off"
-                strip.off()
+    def gerald(self):
+        while (True):
+            strip.rainbow()
 
-            elif (op == self.THEATER_CHASE):
-                print "Sending kitchen theater chase"
-                while (True):
-                    strip.theaterChase(strip.BRIGHT_WHITE)
-                    strip.theaterChase(strip.RED)
-                    strip.theaterChase(strip.BLUE)
-                    strip.theaterChase(strip.GREEN)
+    def color_wipe(self):
+        while (True):
+            strip.colorWipe(strip.BLUE)
+            strip.colorWipe(strip.RED)
+            strip.colorWipe(strip.GREEN)
+            strip.colorWipe(strip.BRIGHT_WHITE)
 
-            elif (op == self.RAINBOW):
-                print "Sending kitchen rainbow"
-                while (True):
-                    strip.rainbow()
+    def m_and_b(self):
+        strip.custom({1: (0, 0, 255,),
+                      2: (255,255,0,),
+                      3: (0, 0, 255,),
+                      4: (255,255,0,)})
 
-            elif (op == self.COLOR_WIPE):
-                print "Sending kitchen color wipe"
-                while (True):
-                    strip.colorWipe(strip.BLUE)
-                    strip.colorWipe(strip.RED)
-                    strip.colorWipe(strip.GREEN)
-                    strip.colorWipe(strip.BRIGHT_WHITE)
+    def seizure(self):
+        while (True):
+            strip.strobe()
 
-            else:
-                print "Sending nothing atm"
+    def on(self):
+        strip.on()
 
-        else:
-            print "Sending off"
-            strip.off()
+    def off(self):
+        strip.off()
+
+    lookup = {
+        "on": on,
+        "off": off,
+        "mandb": m_and_b,
+        "seizure": seizure,
+        "chase": theater_chase,
+        "gerald": gerald,
+        "wipe": color_wipe
+    }
