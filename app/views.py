a150865d776ef3d898e18ef76f2175dae2eda293
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-11-21 22:36:28 -0500

    add initial on/off floating buttons for mobile

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, request, session, url_for
 from app import app
 import time
 from multiprocessing import Process
-from LED_strip import strip, p, handler
+#from LED_strip import strip, p, handler
 
 
 @app.route('/')
@@ -13,13 +13,13 @@ def index():
 
 @app.route('/signal/<opcode>')
 def signal(opcode):
-    global p
-    if (p.is_alive()):
-        print "Killing process"
-        p.terminate()
+    #global p
+    #if (p.is_alive()):
+    #    print "Killing process"
+    #    p.terminate()
 
-    p = Process(target=send_signal, args=(opcode,))
-    p.start()
+    #p = Process(target=send_signal, args=(opcode,))
+    #p.start()
 
     # return 0 from main in pixelate_send.cpp so anything non-zero
     # prolly means we encountered errors
