05894867914657faf8cb9375da82621a7786047d
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-03-06 05:15:59 -0500

    TOFIX: does not properly start the dash server process.

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@
 # find the server that is running and kill the process
 ps aux | grep 'main.py' | sudo kill $(awk '{print $2}')
 
+
+# restart the dash_button listener
+#../dash_button/run.sh
+
 # re-run the server
 nohup sudo ./main.py &
