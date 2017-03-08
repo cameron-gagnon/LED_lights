2bdc9b31f0165880bbb30ce02824cdfa96b70f7a
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-03-07 21:13:34 -0500

    start other run.sh files from this master script

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # find the server that is running and kill the process
 ps aux | grep 'main.py' | sudo kill $(awk '{print $2}')
 
 
 # restart the dash_button listener
-#../dash_button/run.sh
+# since pushd isn't supported in sh
+home_dir=$(pwd)
+cd ../dash_button
+./run.sh
+
+cd ../alexa_lumen
+./run.sh
+
+cd "$home_dir"
 
 # re-run the server
 nohup sudo ./main.py &
