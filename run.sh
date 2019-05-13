e092e73b46775ddaebbabc47d9ad323c7741a37b
Cameron Gagnon <cameron.gagnon@gmail.com>
2019-05-13 01:43:56 -0700

    Add botify run.sh to main run.sh file

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 # find the server that is running and kill the process
-ps aux | grep 'main.py' | tee /dev/tty | sudo kill $(awk '{print $2}')
+ps -U root u | grep main.py | tee /dev/tty | sudo kill $(awk '{print $2}')
 
 # restart the dash_button listener
 # since pushd isn't supported in sh
 home_dir=$(pwd)
-cd ../dash_button
-./run.sh
+#cd ../dash_button
+#./run.sh
 
-cd ../alexa_lumen
-./run.sh
+#cd ../alexa_lumen
+#./run.sh
 
 cd ../lightbot
+./run.sh
+
+cd ../botify
 ./run.sh
 
 cd "$home_dir"
