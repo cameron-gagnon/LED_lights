7e0a30623eec667f7d3371c38a0d9f7615b0726d
Cameron Gagnon <cameron.gagnon@gmail.com>
2020-01-21 00:04:41 -0800

    Clean up run.sh so it only restarts pixelated

--- run.sh
+++ run.sh
@@ -3,23 +3,6 @@
 # find the server that is running and kill the process
 ps -U root u | grep main.py | tee /dev/tty | sudo kill $(awk '{print $2}')
 
-# restart the dash_button listener
-# since pushd isn't supported in sh
-home_dir=$(pwd)
-#cd ../dash_button
-#./run.sh
-
-#cd ../alexa_lumen
-#./run.sh
-cd ../chatbot
-./run.sh
-
-#cd ../botify
-#./run.sh
-
-cd "$home_dir"
-
 # re-run the server
 echo "Running main server...\n"
-export PYTHONUNBUFFERED=1
-nohup sudo ./main.py &
+PYTHONUNBUFFERED=1 nohup sudo ./main.py &
