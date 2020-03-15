480a1e15ceaebfad33029ff88a64f1ac5c24cdea
Cameron Gagnon <cameron.gagnon@gmail.com>
2020-03-15 15:52:16 -0700

    Update command that kills old pixelated processes
    Also change the name from 'main.py' to 'pixelated.py' so it doesn't
    accidentally kill other processes running with 'main.py'

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+PROGRAM_NAME="./pixelated.py"
 # find the server that is running and kill the process
-ps -U root u | grep main.py | tee /dev/tty | sudo kill $(awk '{print $2}')
+#ps -U root u | grep main.py | tee /dev/tty | sudo kill $(awk '{print $2}')
+sudo pkill -f $PROGRAM_NAME
 
 # re-run the server
 echo "Running main server...\n"
-PYTHONUNBUFFERED=1 nohup sudo ./main.py &
+PYTHONUNBUFFERED=1 nohup sudo $PROGRAM_NAME &
